0187d9af087489b10be2acbbc84af7ae2b1b07e9
kevinp1234 <kevin_paavo@hotmail.com>
2024-04-09 13:36:23 +0300

    DB stuff

--- database/1_reset_database.sql
+++ database/1_reset_database.sql
@@ -1,7 +1,7 @@
 -- Kustutab public schema (mis põhimõtteliselt kustutab kõik tabelid)
-DROP SCHEMA public CASCADE;
+DROP SCHEMA manager CASCADE;
 -- Loob uue public schema vajalikud õigused
-CREATE SCHEMA public
+CREATE SCHEMA manager
 -- taastab vajalikud andmebaasi õigused
-    GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO PUBLIC;
+    GRANT ALL ON SCHEMA manager TO postgres;
+GRANT ALL ON SCHEMA manager TO PUBLIC;
