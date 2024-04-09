8daacf5c08f22a7f2a05c2236b77b9c2ee74132c
kevinp1234 <kevin_paavo@hotmail.com>
2024-04-09 13:35:10 +0300

    Initial commit

---
+++ src/main/java/com/football/managerback/ManagerbackApplication.java
@@ -0,0 +1,13 @@
+package com.football.managerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/football/managerback/ManagerbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.football.managerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
