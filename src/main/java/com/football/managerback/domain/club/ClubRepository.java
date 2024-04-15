5b91111d1d363ef3f40e6a1ab3187dac9dd7b666
kevinp1234 <kevin_paavo@hotmail.com>
2024-04-15 14:52:14 +0300

    ClubInfo dto refactor

--- src/main/java/com/football/managerback/domain/club/ClubRepository.java
+++ src/main/java/com/football/managerback/domain/club/ClubRepository.java
@@ -1,9 +1,7 @@
 package com.football.managerback.domain.club;
 
-import com.football.managerback.domain.club.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
--- src/main/java/com/football/managerback/manager/club/ClubController.java
+++ src/main/java/com/football/managerback/manager/club/ClubController.java
@@ -1,7 +1,6 @@
 package com.football.managerback.manager.club;
 
 import com.football.managerback.manager.club.dto.ClubInfo;
-import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,10 +15,6 @@ public class ClubController {
     private ClubService clubService;
 
     @GetMapping("/clubs/{leagueId}")
-    @Operation(
-            summary = "Leiab süsteemist (andmebaasist country tabelist) kõik riigid confederation ID kaudu.",
-            description = "tagastab info CountryName'ga"
-    )
     public List<ClubInfo> getClubs(@PathVariable Integer leagueId) {
         return clubService.getClubs(leagueId);
 
