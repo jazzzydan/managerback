fe07c0daf0e5b619f2ecab694176743c0499baca
Daniil Starov <jazzzydan@hotmail.com>
2024-04-15 12:25:33 +0300

    confederations dropdown service added

--- src/main/java/com/football/managerback/domain/club/confederation/ConfederationRepository.java
+++ src/main/java/com/football/managerback/domain/club/confederation/ConfederationRepository.java
@@ -1,9 +1,6 @@
 package com.football.managerback.domain.club.confederation;
 
-import com.football.managerback.domain.club.confederation.Confederation;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface ConfederationRepository extends JpaRepository<Confederation, Integer> {
 }
